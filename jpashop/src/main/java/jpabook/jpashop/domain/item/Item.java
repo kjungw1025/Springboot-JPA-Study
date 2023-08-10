3df346ee5d2257419e0b16502d66a62f123f627e
kjungw1025 <kjungw1025@naver.com>
2023-08-10 19:00:44 +0900

    feat: Update Domain item entity
    Added business logic

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 데이터(price, stockQuantity)를 가지고 있는 쪽의 비즈니스 메서드가 있는게 가장 좋음 -> 응집력 있음
+    //==비즈니스 로직==//
+    /**
+     *  stock 증가
+     */
+    public void addStock (int quantity) {
+        this.stockQuantity += quantity;
+    }
+    // stockQuantity를 변경할 일이 있으면 @Setter를 사용하지 않고, addStock, removeStock과 같은 비즈니스 메서드를 통해 변경해야 함
+
+    /**
+     *  stock 감소
+     */
+    public void removeStock (int quantity) {
+        int restStock = this.stockQuantity -= quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
