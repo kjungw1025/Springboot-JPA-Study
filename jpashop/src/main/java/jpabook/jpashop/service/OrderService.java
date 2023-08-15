d3fddc58f407c4de291e5f9ee65119c01a81e1ac
kjungw1025 <kjungw1025@naver.com>
2023-08-15 22:27:06 +0900

    feat: Create Order service test code

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -19,7 +19,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final MemberRepository memberRepository;
-    private final ItemRepository itemRepository
+    private final ItemRepository itemRepository;
 
     /**
      * 주문
